110114a658b1b104329d0a952d3e3a8327b92321
Abhi Sharma <me.avish143@gmail.com>
2024-07-27 01:57:32 +0530

    overflow issue

--- script.js
+++ script.js
@@ -3,14 +3,11 @@ const openBtn = document.querySelector("#navOpenBtn")
 const navLinks = document.querySelector(".nav-links")
 
 
-
-openBtn.addEventListener("click" , ()=>
-{
-    navLinks.classList.add('right-0');
-    navLinks.classList.remove('right-[-500%]');
+openBtn.addEventListener("click", () => {
+    navLinks.style.right = "0";
 })
-closeBtn.addEventListener("click" , ()=>{
-    navLinks.classList.add('right-[-500%]');
-    navLinks.classList.remove('right-0');
+
+closeBtn.addEventListener("click", () => {
+    navLinks.style.right = "-500%";
 
 })
