adfa943fdb602ad6279fd0ac5279effd384aa8e0
Abhi Sharma <me.avish143@gmail.com>
2024-07-28 01:28:20 +0530

    Products page

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const closeBtn = document.querySelector("#navCloseBtn")
 const openBtn = document.querySelector("#navOpenBtn")
 const navLinks = document.querySelector(".nav-links")
+const smallImages = document.querySelectorAll(".small-img img")
+const mainImg = document.querySelector("#main-img");
 
 
 openBtn.addEventListener("click", () => {
@@ -11,3 +13,15 @@ closeBtn.addEventListener("click", () => {
     navLinks.style.right = "-500%";
 
 })
+
+/* Products section image changing*/
+
+smallImages.forEach((img)=>{
+    img.addEventListener("click" , ()=>{
+        let link = mainImg.getAttribute("src");
+        mainImg.src = img.src;
+        img.src =link;
+    })
+})
+
+
